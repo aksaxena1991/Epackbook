2d3304bc8cdd94a37cc9b1b890c46c9c844d026e
Anubhav Saxena <aksaxena1991@gmail.com>
2026-02-25 23:15:54 +0530

    auth model updated

--- app/models/auth.py
+++ app/models/auth.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime,Boolean
 from datetime import datetime
 from app.db.database import Base
 
@@ -6,8 +6,12 @@ from app.db.database import Base
 class Auth(Base):
     __tablename__ = "auth"
     __table_args__ = {"schema": "auth_schema"}
+
     id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String, nullable=False)
+
+    provider = Column(String, default='internal', nullable=False)
     password = Column(String, nullable=False)
+    
+    is_active = Column(Boolean, default=True)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
-    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow)
